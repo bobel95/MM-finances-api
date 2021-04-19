506b44ee8f7fc1f312c1faf8e9471a6bbd212ec6
Iulian Popa <bobel2007@gmail.com>
2021-04-19 15:25:11 +0300

    Add Payment model

---
+++ src/main/java/com/mihmih/finances/model/Payment.java
@@ -0,0 +1,32 @@
+package com.mihmih.finances.model;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+
+import org.javamoney.moneta.Money;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+
+public class Payment {
+
+    @Id
+    @SequenceGenerator(name = "payment_sequence",
+            sequenceName = "payment_sequence",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "payment_sequence")
+    private Long id;
+
+    private Money money;
+    private Date date;
+    private PaymentCategory paymentCategory;
+
+}
