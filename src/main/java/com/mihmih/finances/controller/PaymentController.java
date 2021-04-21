e91f6c00a0c1660a2b88608447af6c264c7f79e4
Iulian Popa <bobel2007@gmail.com>
2021-04-21 22:19:16 +0300

    Add relationship between entities

--- src/main/java/com/mihmih/finances/controller/PaymentController.java
+++ src/main/java/com/mihmih/finances/controller/PaymentController.java
@@ -2,8 +2,11 @@ package com.mihmih.finances.controller;
 
 import com.mihmih.finances.model.Payment;
 import com.mihmih.finances.model.PaymentCategory;
+import com.mihmih.finances.repository.AppUserRepository;
 import com.mihmih.finances.repository.PaymentRepository;
 import static com.mihmih.finances.specification.PaymentSpecification.*;
+
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.data.jpa.domain.Specification.*;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -19,14 +22,11 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/payment")
+@AllArgsConstructor
 public class PaymentController {
 
     private final PaymentRepository paymentRepository;
-
-    @Autowired
-    public PaymentController(PaymentRepository paymentRepository) {
-        this.paymentRepository = paymentRepository;
-    }
+    private final AppUserRepository appUserRepository;
 
     @GetMapping
     public List<Payment> getPayments(
@@ -49,6 +49,8 @@ public class PaymentController {
 
     @PostMapping
     public Payment addPayment(@RequestBody Payment payment) {
+        // TODO: refactor addPayment endpoint
+        payment.setAppUser(appUserRepository.getOne(payment.getAppUser().getId()));
         return paymentRepository.save(payment);
     }
 }
